f07aa5d3cb58798c425076654ee3dcdde2da705a
apJammanbo <apJammanbo@gmail.com>
2018-05-05 15:40:44 +0900

    shoutem 적

---
+++ 9oclock/src/components/ShoutemExample.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Card, Image, Subtitle, Caption, Examples} from '@shoutem/ui';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Examples/>
+        );
+    }
+}
